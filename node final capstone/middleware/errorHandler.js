3edf50aa808c81fed53f7d823789da5a29ac600f
ravanyarenganathan <145009142+ravanyarenganathan@users.noreply.github.com>
2024-09-16 20:18:14 +0530

    Delete node final capstone directory

--- node final capstone/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const { STATUS_CODE } = require("../const/httpStatusCode");
-
-const errorHandler = (err, req, res, next) => {
-  res
-    .status(STATUS_CODE.CLIENT_ERROR_RESPONSE_CODE)
-    .json({ message: err.message });
-};
-
-module.exports = { errorHandler };
--- node final capstone/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { addUser, getUsersDetail } = require("../controllers/userController");
-const router = express.Router();
-
-router.route("/").get(getUsersDetail).post(addUser);
-
-module.exports = router;
