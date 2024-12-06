b7ea06a005a37859ba7108a01afd613c9c4aa286
ravanyarenganathan <145009142+ravanyarenganathan@users.noreply.github.com>
2024-12-06 15:03:09 +0530

    Adding node prisma sqllite project

---
+++ Capstone_Node_Prisma_sqlite/prisma/migrations/20240920010129_update_user_add_exercise_relation/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Exercise" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Exercise_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
